1572956c9bd955219b2ec44dcbe43021192dfacc
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-03-20 08:28:45 +0100

    Import css and bit of text

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,11 @@ const Home: NextPage = () => {
     return (
         <>
             <GlobalHead/>
+            <div className="h-screen">
+                <div className="flex flex-col items-center justify-center h-full">
+                    <h1 className="text-6xl font-bold">Hi! I&apos;m TiMyTi11</h1>
+                </div>
+            </div>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                background: "#0061FF",
-                section: "#4685F3",
-                content: "#38D8E2",
+                background: "#0085F2",
+                section: "#FF00CF",
+                content: "#0896BC",
                 text: "#F7EBF6"
             },
         },
